7660b770a29d435abe63e4ba15c99dd78b3179ef
Sicelumusa Gabuza <musaqwabe@gmail.com>
2025-06-13 17:26:19 +0200

    Moved html to templates and added a validator endpoint to use the frontend validator

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from rearange import re_arranger
 
 
 def create_app():
    
-    app = Flask(__name__)
+    app = Flask(
+        __name__, 
+        static_folder='static', 
+        template_folder='templates'
+    )
 
     CORS(app)
+
+    @app.route('/validate')
+    def serve_frontend():
+        return render_template('index.html')
     
     app.register_blueprint(re_arranger)
     return app
